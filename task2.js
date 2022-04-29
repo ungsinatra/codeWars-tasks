f6d50f071ffae7ce923b1117e5b29d06579a00cf
ungsinatra <mamirov.21@mail.ru>
2022-04-29 21:44:01 +0300

    task2 done

--- task2.js
+++ task2.js
@@ -1,16 +1,20 @@
 //! https://www.codewars.com/kata/523f5d21c841566fde000009/train/javascript
 
-
 function arrayDiff(a, b) {
-	const arrOfB =b;
-	const newArr =[]
-	a.filter(el =>{
-		if(el!==arrOfB[0]&&el!==arrOfB[1]){
-			newArr.push(el);
-		}else{
-			return []
+  const newArr = [];
+  for (let i = 0; i < a.length; i++) {
+	  if(b.length>1){
+		  if(a[i]!=b[i]){
+			  newArr.push(a[i])
+		  }
+	  }else {
+		if(a[i]!=b){
+			newArr.push(a[i]);
 		}
-	})
-	return newArr
+		
+	  }
+  }
+
+  return newArr;
 }
-console.log(arrayDiff([1,2,3], [1,2]))
+console.log(arrayDiff([1,2,3], [1,2]));
