ebdf2cf84fb39f5ba77148f3a010d0be421dc946
ungsinatra <mamirov.21@mail.ru>
2022-05-05 23:02:28 +0300

    task 4 fixed

--- task4.js
+++ task4.js
@@ -1,44 +1,13 @@
 // https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/javascript
+var moveZeros = function (arr) {
+  let zeroOfArr = arr.filter((elm) => elm === 0).length;
+  let res = arr.filter((elm) => elm !== 0);
 
-function moveZeros(arr) {
-	let elZero =[]
-	let strInArr = []
-	let boolOfArr = [];
-	let nuberOfarr = []
-	let newArr = arr.filter((el)=>{
-		if(typeof el=='boolean'){
-			boolOfArr.push(el)
-		}
-		if(el==0){
-			elZero.push(el)
-		}
-		if(typeof el =='string'){
-			strInArr.push(el);
-		}
-		if(el>0){
-			nuberOfarr.push(el)
-		}
+  for (let i = 0; i < zeroOfArr; i += 1) {
+    res.push(0);
+  }
 
-	})
-	boolOfArr.map(el=>{
-		return newArr.push(el)
+  return res;
+};
 
-	})
-	nuberOfarr.map(el=>{
-
-		return newArr.push(el)
-	})
-	strInArr.map((el)=>{
-		return newArr.push(el)
-
-	})
-	elZero.map((el)=>{
-		return newArr.push(el)
-
-	})
-	
-	return newArr
-}
-
-
-console.log(moveZeros([false,'a','asd','ssss',1,2,0,1,0,,0,0,1]))
+console.log(moveZeros());
