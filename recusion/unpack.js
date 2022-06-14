4bc465546a0c8a2416a1b6191f1506d9f328a69b
ungsinatra <mamirov.21@mail.ru>
2022-06-14 10:00:25 +0300

    dell comments

--- recusion/unpack.js
+++ recusion/unpack.js
@@ -1,23 +1,75 @@
-let newArr = []
-function unpack(arr,output){
-	for(let item of arr){
-		if(Array.isArray(item)){
 
-			unpack(item,output)
-		}
-		else if(item instanceof Object){
-			unpack(Object.values(item),output)
-		}
-		else{
+let newArr = [];
+function unpack(arr, output) {
+  for (let item of arr) {
+    if (Array.isArray(item)) {
+      unpack(item, output);
+    } else if (item instanceof Object) {
+      unpack(Object.values(item), output);
+    } else {
+      output.push(item);
+    }
+  }
 
-			output.push(item)
-		}
-		
-	}
-
-	return output
-
+  return output;
 }
 
+console.log(unpack([{ name: "sardor" }, [1, 2, 3], ["bbb"]], newArr)); //['sardor',1,2,3,bbb]
 
-console.log(unpack([{name:'sardor'},[1,2,3],['bbb']],newArr))//['sardor',1,2,3,bbb]
+//---------------------------------------------------------------------------------
+
+function sequenSum(a, b) {
+  if (a == b) {
+    return a;
+  } else if (a > b) {
+    return NaN;
+  }
+  return b + sequenSum(a, b - 1);
+}
+
+console.log(sequenSum(7, 7));
+
+//----------------------------------------------------------------------
+
+
+
+
+
+
+function getStingCount(obj) {
+  let count = 0;
+
+	if (obj instanceof Object) {
+		for (let value of Object.values(obj)) {
+		  if (typeof value === "string") {
+			count += 1;
+		  }else{
+	
+			  count += getStingCount(value);
+		  }
+		}
+	  }
+	  
+	  return count;
+  }
+
+
+
+
+let obj = {
+	first: "1",
+	second: "2",
+	third: false,
+	fourth: ["somthin", "2", 2],
+	fifth: null,
+	sixth: {
+	  somthin: "asdasd",
+	  alll: "true",
+	},
+  };
+
+
+let getValue = getStingCount(obj)
+console.log(getValue)
+// console.log(getStingCount({ first: "1", second: "2", third: false, fourth: [1, 2, "Goof"] }));
+// console.log(getStingCount({ first: "1", second: "2", third: false, fourth: [1, 2, "Goof"] }));
